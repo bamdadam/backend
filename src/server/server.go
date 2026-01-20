c9b9379dc0dca04abdff97a41e01ab074c4ec574
Bamdad <kanra252@gmail.com>
2026-01-20 20:38:12 +0100

    feat: add base graphql schema and codegens

--- src/server/server.go
+++ src/server/server.go
@@ -4,19 +4,24 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"time"
 
-	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/bamdadam/backend/graph"
+	"github.com/vektah/gqlparser/v2/ast"
 )
 
 // Run initializes and starts the GraphQL server
 func Run(ctx context.Context, db *sql.DB, addr string) error {
-	graphqlHandler := newGraphQLHandler()
+	graphqlHandler := newGraphQLHandler(db)
 	healthHandler := newHealthHandler(db)
-	
+
 	http.Handle("/graphql", graphqlHandler)
 	http.Handle("/health", healthHandler)
 
@@ -27,7 +32,7 @@ func Run(ctx context.Context, db *sql.DB, addr string) error {
 	}
 
 	go func() {
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 			log.Printf("Server error: %v", err)
 		}
 	}()
@@ -41,9 +46,23 @@ func Run(ctx context.Context, db *sql.DB, addr string) error {
 	return server.Shutdown(shutdownCtx)
 }
 
-func newGraphQLHandler() http.Handler {
-	srv := handler.NewDefaultServer(&graphql.ExecutableSchemaMock{})
-	// Build your solution here and replace the mock schema above
+func newGraphQLHandler(db *sql.DB) http.Handler {
+	resolver := &graph.Resolver{
+		DB: db,
+	}
+	srv := handler.New(graph.NewExecutableSchema(
+		graph.Config{Resolvers: resolver}),
+	)
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Websocket{})
+	srv.SetQueryCache(lru.New[*ast.QueryDocument](1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New[string](100),
+	})
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		srv.ServeHTTP(w, r)
